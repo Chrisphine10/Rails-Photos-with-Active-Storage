d8602d7823ee43e64fd85ecc32a6afa627e443cc
Chrisphine10 <ondiekchrisphine@gmail.com>
2020-05-23 03:54:34 +0300

    Active Storage

---
+++ db/migrate/20200523001346_create_photos.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreatePhotos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :photos do |t|
+      t.string :title
+      t.text :caption
+
+      t.timestamps
+    end
+  end
+end
